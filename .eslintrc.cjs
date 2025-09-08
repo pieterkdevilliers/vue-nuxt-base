a8e2db3a52b9f41eee80c9b5454399e5d7e30e29
Pieter K de Villiers <pieter@hey.com>
2025-09-08 09:37:39 +0100

    Set up baseline packages

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // Example: your custom tweaks
+    'vue/multi-word-component-names': 'off'
+  }
+}
